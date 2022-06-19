e9e7ca1e717b4695a60769887ef1701ee6ceef03
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-06-19 16:14:25 +0530

    navbar fully completed

---
+++ frontend/src/components/Annoncement.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 30px;
+background-color: pink;
+color:navy;
+display:flex;
+align-items:center;
+justify-content:center;
+font-Size:15px;
+font-weight:500
+
+`
+
+const Annoncement = () => {
+  return (
+    <Container>
+        Super Deal! Free Shipping on Orders Over $50
+    </Container>
+  )
+}
+
+export default Annoncement
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Annoncement from '../components/Annoncement'
 import Navbar from '../components/Navbar'
 
 
 const Home = () => {
   return (
     <>
+    <Annoncement/>
         <Navbar/>
     </>
   )
